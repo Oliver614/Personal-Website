4bc33bfe150c5ed15356eef5744547e582fb33f0
Oliver614 <oliver61@live.co.uk>
2022-04-08 15:20:45 +0100

    Update EnterScreen.js

--- src/Pages/Main/EntryScreen/EnterScreen.js
+++ src/Pages/Main/EntryScreen/EnterScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './EnterScreen.css';
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
 import {motion} from 'framer-motion';
 import { SocialIcon } from 'react-social-icons';
 
@@ -29,11 +29,12 @@ class EnterScreen extends React.Component
                         exit={{ y: 0, opacity: 0, transition: {duration: 1.5}}}
                         
                         className='Enter'>
-                <h1>Hi, I'm Oliver</h1>
-
+                <h1>Hi, I'm Oliver, my website is currently under construction.</h1>
+                <h1>Please use my social links to contact me, or my github to see my recent projects.</h1>
+                {/*
                 <div className='ButtonContainer'>
 
-
+                    
                     <Link to = "/About" >
                         <motion.button whileHover = {{scale: 1.1}} className = "AboutButton">About</motion.button>
                     </Link>
@@ -43,24 +44,25 @@ class EnterScreen extends React.Component
                     <Link to = "/About" >
                         <motion.button whileHover = {{scale: 1.1}} className = "ContactButton">Contact</motion.button>
                     </Link>
-                </div>
-                
+        
+                </div> 
+                */}           
                 <div className = "socialContainer">
 
                     <motion.button className = "socialButton" whileHover = {{scale: 1.05}} >
-                        <SocialIcon className = "socialIcon" url="https://linkedin.com" target="_blank"/>
+                        <SocialIcon className = "socialIcon" url="https://linkedin.com/in/oliver-hodgson-72a170161/" target="_blank"/>
                     </motion.button>
 
                     <motion.button className = "socialButton" whileHover = {{scale: 1.05}} >
-                        <SocialIcon className = "socialIcon" url="https://facebook.com" target="_blank"/>
+                        <SocialIcon className = "socialIcon" url="https://facebook.com/oliver.hodgson.50/" target="_blank"/>
                     </motion.button>
 
                     <motion.button className = "socialButton" whileHover = {{scale: 1.05}} >
-                        <SocialIcon className = "socialIcon" url="https://github.com" target="_blank"/>
+                        <SocialIcon className = "socialIcon" url="https://github.com/oliver614" target="_blank"/>
                     </motion.button>
 
                     <motion.button className = "socialButton" whileHover = {{scale: 1.05}} >
-                        <SocialIcon className = "socialIcon" url="https://instagram.com" target="_blank"/>
+                        <SocialIcon className = "socialIcon" url="https://instagram.com/hodgson_oliver" target="_blank"/>
                     </motion.button>
                 </div>    
             </motion.div>
